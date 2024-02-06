687e02a7df079adf432a09c119f0e56fbbd4d2a4
IvanJavDev <ivankarataevvv@gamil.com>
2024-02-06 16:03:57 +0300

    new branch created

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    Mage mage = new Mage("Shkipper", 10 , 100 , "earth");
-        Mage mage2 = new Mage("Kovalskyi", 5 , 50 , "ice");
-        Mage mage3 = new Mage("Rico", 15 , 150 , "Fire");
+        System.out.println("new branch");
     }
 }
